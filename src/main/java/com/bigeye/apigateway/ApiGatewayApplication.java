5445564342c7040b9901c5ef91099776ac9f3b68
iamrkgolu <iamrkgolu@gmail.com>
2023-10-04 10:48:18 +0530

    Code refactor

--- src/main/java/com/bigeye/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/bigeye/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,10 @@ package com.bigeye.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +15,7 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+
 
 }
